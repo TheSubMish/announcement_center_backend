42c3dd280098e430aa273e0a3724a70cfe36778d
Subodh <subodhmishra015@gmail.com>
2024-06-03 16:09:55 +0545

    add: mongodb configutaion

---
+++ src/apps/notification/mongodb.py
@@ -0,0 +1,18 @@
+from pymongo.mongo_client import MongoClient
+from bson.objectid import ObjectId
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+
+class MongoDB:
+    _connection_string = ""
+    
+    def __init__(self):
+        self._connection_string =os.environ.get("MONGODB_CONNECTION_STRING")
+
+    def connect_db(self, db_name):
+        print(self)
+        return MongoClient(self._connection_string)[db_name]
+
+database = MongoDB()
