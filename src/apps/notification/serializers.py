9c2fc898d2ac6c0dd992cf95349b777514e96972
submish <subodhmishra015@gmail.com>
2024-07-28 13:06:15 +0545

    feat: notification read update

--- src/apps/notification/serializers.py
+++ src/apps/notification/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 
 class NotificationSerializer(serializers.Serializer):
-
+    _id = serializers.UUIDField()
     sender = serializers.UUIDField()
     receiver = serializers.UUIDField()
     message = serializers.CharField(max_length=255)
     read = serializers.BooleanField(default=False)
     created_at = serializers.DateTimeField()
+
+class ReadNotificationSerializer(serializers.Serializer):
+    notification_id = serializers.UUIDField()
+    read = serializers.BooleanField(default=True)
--- src/apps/notification/views.py
+++ src/apps/notification/views.py
@@ -1,9 +1,10 @@
 from rest_framework import generics,permissions,status,exceptions
 from rest_framework.response import Response
-from .serializers import NotificationSerializer
+from .serializers import NotificationSerializer,ReadNotificationSerializer
 from django.conf import settings
 from .mongodb import database
 from pymongo import DESCENDING
+from uuid import UUID
 
 import logging
 logger = logging.getLogger('error_logger')
@@ -26,3 +27,29 @@ class ListNotificationView(generics.ListAPIView):
 
         serializer = NotificationSerializer(notifications, many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    
+class ReadNotificationView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = ReadNotificationSerializer
+
+    def patch(self, request, *args, **kwargs):
+        notification_id = request.data.get('notification_id')
+        read = request.data.get('read')
+
+        try:
+            db = database.connect_db(settings.MONGODB)
+        except Exception as e:
+            logger.error(f"Failed to connect to MongoDB: {e}")
+            raise exceptions.APIException("Something went wrong")
+        
+        notification = db.notification.find_one_and_update(
+            {"_id": UUID(notification_id)},
+            {"$set": {"read": read}},
+            return_document=True
+        )
+
+        if notification is None:
+            raise exceptions.APIException('Notification not found')
+
+        serializer = NotificationSerializer(notification)
+        return Response(serializer.data, status=status.HTTP_200_OK)
