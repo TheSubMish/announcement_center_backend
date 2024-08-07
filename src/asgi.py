1142f36469e814c3398ef51b759987dc371eca48
submish <subodhmishra015@gmail.com>
2024-08-07 22:55:36 +0545

    feat: notification consumenrs and routing

--- src/asgi.py
+++ src/asgi.py
@@ -1,16 +1,20 @@
-"""
-ASGI config for src project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
 import os
-
+from channels.routing import ProtocolTypeRouter,URLRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from src.apps.notification.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'src.settings')
 
 application = get_asgi_application()
+
+
+application = ProtocolTypeRouter({
+    "http": application,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    )
+})
