b448f98b11869a1e01971e8a8de126e324f214f8
submish <subodhmishra015@gmail.com>
2024-12-23 22:10:25 +0545

    feat: group rate analytics

--- src/apps/analytics/urls.py
+++ src/apps/analytics/urls.py
@@ -2,15 +2,17 @@ from django.urls import path
 
 from .views import (
     GroupImpressionView,
+    GroupImpressionCountryCityView,
+    GroupRateAnalyticsView,
     AnnouncementImpressionView,
     AnnouncementLikeDislikeView,
-    GroupImpressionCountryCityView,
 )
 
 
 urlpatterns = [
     path('group/<uuid:pk>/impression/', GroupImpressionView.as_view(), name='group-impression-api'),
     path('group/<uuid:pk>/impression/country-city/', GroupImpressionCountryCityView.as_view(), name='group-impression-country-city-api'),
+    path('group/<uuid:pk>/rate/', GroupRateAnalyticsView.as_view(), name='group-rate-api'),
     path('announcement/<uuid:pk>/impression/', AnnouncementImpressionView.as_view(), name='announcement-impression-api'),
     path('announcement/<uuid:pk>/like-dislike/', AnnouncementLikeDislikeView.as_view(), name='announcement-like-dislike-api'),
 ]
--- src/apps/analytics/views.py
+++ src/apps/analytics/views.py
@@ -1,12 +1,12 @@
 from rest_framework import generics,permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.db.models import Count,F
+from django.db.models import Count,F,Avg
 from django.db.models.functions import TruncDate
 from django.utils import timezone
 from datetime import timedelta
 
-from src.apps.group.models import AnnouncementGroup, GroupMember
+from src.apps.group.models import AnnouncementGroup, GroupMember, Rating
 from src.apps.announcement.models import Announcement, AnnouncementLike, AnnouncementComment
 
 from .serializers import (
@@ -125,6 +125,63 @@ class GroupImpressionCountryCityView(APIView):
                 .order_by('-count')
             )
             serializer = CountryCitySerializer(impressions_per_city, many=True)
+        
+        return Response(serializer.data)
+    
+
+class GroupRateAnalyticsView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        group_id = kwargs.get('pk')
+
+        try:
+            group = AnnouncementGroup.objects.get(pk=group_id)
+            if not group.premium_group:
+                return Response({'error': 'You do not have permission'}, status=403)
+            
+            if not GroupMember.objects.filter(
+                group=group,
+                user=request.user,
+                role__in=["admin", "moderator"]
+            ).exists():
+                return Response({'error': 'You do not have permission'}, status=403)
+            
+        except AnnouncementGroup.DoesNotExist:
+            return Response({'error': 'Announcement group does not exist'}, status=404)
+
+        range = self.request.query_params.get("range", None) # type: ignore
+
+        if range is None:
+            return Response({'error': 'range is required'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        today = timezone.now()
+        if range == "this_week":
+            # Calculate the date 7 days ago
+            last_date = today - timedelta(days=7)
+        
+        if range == "this_month":
+            # Calculate the date 30 days ago
+            last_date = today - timedelta(days=30)
+        
+        if range == "3_months":
+            # Calculate the date 90 days ago
+            last_date = today - timedelta(days=90)
+        
+        if range == "all_time":
+            # Calculate the date 0 days ago
+            last_date = timezone.datetime(1970, 1, 1)
+
+        # Query to find average rating in per day
+        average_rating_per_day = (
+            Rating.objects
+            .filter(group=group, created_at__gte=last_date, created_at__lte=today)
+            .annotate(date=TruncDate('created_at'))
+            .values('date')
+            .annotate(count=Avg('rating'))
+            .order_by('date')
+        )
+        serializer = ImpressionSerializer(average_rating_per_day, many=True)
         
         return Response(serializer.data)
 
